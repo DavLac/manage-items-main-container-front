625c8c3d3fa7d76cf6b429a1399103cdc9252728
David.Lacoste <david.lacoste2@gmail.com>
2021-05-11 11:57:09 +0200

    Clean + add search & items management components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 import User from '@bit/davlac.manage-items.user';
+import Search from '@bit/davlac.manage-items.search';
+import Items from '@bit/davlac.manage-items.items';
 
-function App() {
+const App = () => {
     return (
         <div className="Container">
             <h1>Container</h1>
             <div className="Children">
                 <User className="Child"/>
-                <User className="Child"/>
-                <User className="Child"/>
+                <Search className="Child"/>
+                <Items className="Child"/>
             </div>
         </div>
-
     );
 }
 
